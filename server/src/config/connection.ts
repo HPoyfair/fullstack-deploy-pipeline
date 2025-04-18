52437faa62922d995b47525313557cee8488b77e
HPoyfair <haydenpoyfair@gmail.com>
2025-04-17 21:01:57 -0600

    code

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGODB_URI!)
+  .then(() => console.log('✅ MongoDB connected successfully.'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 export default mongoose.connection;
